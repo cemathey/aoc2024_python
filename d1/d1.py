5f98a521b296ee86e685282a50344422a611e9a2
C. Eric Mathey <emathey@protonmail.com>
2024-12-08 15:59:30 -0700

    D1 P2

--- d1/d1.py
+++ d1/d1.py
@@ -1,7 +1,8 @@
 import sys
+from collections import defaultdict
 
 
-def main():
+def load_list():
     with open(sys.argv[1]) as fp:
         lines = fp.readlines()
 
@@ -13,10 +14,15 @@ def main():
         l2.append(int(right))
 
     assert len(l1) == len(l2)
+    return l1, l2
 
+
+def main():
+    l1, l2 = load_list()
     deltas = calc_deltas(l1, l2)
+    score = calc_similarity(l1, l2)
 
-    return sum(d for d in deltas)
+    return sum(d for d in deltas), score
 
 
 def calc_deltas(l1: list[int], l2: list[int]):
@@ -27,9 +33,21 @@ def calc_deltas(l1: list[int], l2: list[int]):
     return deltas
 
 
+def calc_similarity(l1: list[int], l2: list[int]):
+    counts = defaultdict(int)
+    for num in l2:
+        counts[num] += 1
+
+    return sum(num * counts[num] for num in l1)
+
+
 if __name__ == "__main__":
     deltas = calc_deltas([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3])
     assert sum(d for d in deltas) == 11
 
-    distance = main()
-    print(f"{distance=}")
+    score = calc_similarity([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3])
+    assert score == 31
+
+    distance, score = main()
+    print(f"Part 1 {distance=}")
+    print(f"Part 2 {score=}")
