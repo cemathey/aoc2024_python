735cd867be38d53698ac129a556ab3f495f66f6a
C. Eric Mathey <emathey@protonmail.com>
2024-12-08 17:16:58 -0700

    D2 P2

--- d2/d2.py
+++ d2/d2.py
@@ -16,6 +16,9 @@ def main():
     total = sum(1 for level in lines if is_safe(level))
     print(f"Part 1 {total=}")
 
+    total = sum(1 for level in lines if is_safe_p2(level))
+    print(f"Part 2 {total=}")
+
 
 def is_safe(level: list[int], min_dist=1, max_dist=3) -> bool:
     deltas: list[int] = []
@@ -24,7 +27,7 @@ def is_safe(level: list[int], min_dist=1, max_dist=3) -> bool:
 
     first_delta = None
     for d in deltas:
-        if not first_delta:
+        if first_delta is None:
             first_delta = d
 
         if not min_dist <= abs(d) <= max_dist:
@@ -39,6 +42,18 @@ def is_safe(level: list[int], min_dist=1, max_dist=3) -> bool:
     return True
 
 
+def is_safe_p2(level: list[int], min_dist=1, max_dist=3) -> bool:
+    if is_safe(level, min_dist=min_dist, max_dist=max_dist):
+        return True
+
+    for remove in range(len(level)):
+        combo: list[int] = [num for idx, num in enumerate(level) if idx != remove]
+        if is_safe(combo):
+            return True
+
+    return False
+
+
 if __name__ == "__main__":
     main()
 
@@ -49,3 +64,11 @@ if __name__ == "__main__":
     assert not is_safe([8, 6, 4, 4, 1])
     assert is_safe([1, 3, 6, 7, 9])
     assert is_safe([])
+
+    assert is_safe_p2([7, 6, 4, 2, 1])
+    assert not is_safe_p2([1, 2, 7, 8, 9])
+    assert not is_safe_p2([9, 7, 6, 2, 1])
+    assert is_safe_p2([1, 3, 2, 4, 5])
+    assert is_safe_p2([8, 6, 4, 4, 1])
+    assert is_safe_p2([1, 3, 6, 7, 9])
+    assert is_safe_p2([])
